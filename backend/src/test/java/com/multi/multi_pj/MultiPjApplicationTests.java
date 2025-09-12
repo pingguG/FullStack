040888ecd3a34a9d3c1ba8bf9607e7652d11966f
pingguG <stargate1327@naver.com>
2025-09-12 21:12:50 +0900

    setting

---
+++ backend/src/test/java/com/multi/multi_pj/MultiPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.multi_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
