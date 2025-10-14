9bb4fae9d86d06b5cc39bcd4f0cd5f079a95d201
mingu <stargate1327@naver.com>
2025-10-15 02:29:34 +0900

    successful play

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+// src/App.jsx
+import HomePage from './pages/HomePage'; // 👈 HomePage 불러오기
+
+export default function App() {
+  return (
+    <HomePage /> // 👈 HomePage 렌더링
+  );
+}
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css'; 
+
+export default function Header() {
+  return (
+    <header className="app-header"> 
+      <h1>실행 성공</h1>
+    </header>
+  );
+}
