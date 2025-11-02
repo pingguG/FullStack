bdf4a14711a488d6f327d22d01edb3ef54bbe6b1
pingguG <stargate1327@naver.com>
2025-11-02 16:21:31 +0900

    setting

---
+++ frontend/src/components/HomeSection/Section01.jsx
@@ -0,0 +1,9 @@
+import "./section01.css";
+
+export default function Section01() {
+  return (
+    <section className="home-section01">
+      <h2>섹션 01</h2>
+    </section>
+  );
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,12 +3,14 @@
 import "./homePage.css";
 import Header from "../components/header/Header";
 import Nav from "../components/nav/nav";
+import Section01 from "../components/HomeSection/section01";
 
 export default function HomePage() {
   return (
     <div className="homePage-container">
       <Nav />
       <Header />
+      <Section01 />
     </div>
   );
 }
